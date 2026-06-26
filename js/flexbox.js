4e3afec1fc98d84f1175ca9a3f4cfb22f7fe2fd8
Santander Ricardo <Santanderend@gmail.com>
2026-06-25 23:26:35 -0300

    agragar tarjetas de videojuegos con flexbox

---
+++ js/flexbox.js
@@ -0,0 +1,38 @@
+const videojuegos = [
+    "Minecraft",
+    "GTA V",
+    "Valorant",
+    "Fortnite",
+    "Rocket League",
+    "FIFA"
+];
+
+const contenedor = document.getElementById("contenedor-juegos");
+
+function mostrarJuegos() {
+
+    contenedor.innerHTML = "";
+
+    videojuegos.forEach(function(juego) {
+
+        const tarjeta = document.createElement("div");
+
+        tarjeta.classList.add("tarjeta");
+
+        tarjeta.innerHTML = `
+            <h3>${juego}</h3>
+        `;
+
+        contenedor.appendChild(tarjeta);
+    });
+}
+
+mostrarJuegos();
+
+document.getElementById("btnOrdenar").addEventListener("click", function () {
+
+    videojuegos.sort();
+
+    mostrarJuegos();
+
+});
